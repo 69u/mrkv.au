d10fd3d49a099edf1b05eff16beb6085431803c7
Mr. K V <36492979+69u@users.noreply.github.com>
2023-01-31 17:28:33 +0800

    Update Head.tsx

--- apps/enji.dev/src/components/meta/Head.tsx
+++ apps/enji.dev/src/components/meta/Head.tsx
@@ -21,7 +21,7 @@ function Head({
 
   const htmlTitle = overrideTitle
     ? title
-    : `${title} — Enji · Front-End Developer`;
+    : `${title} — Kyle V · Developer`;
 
   return (
     <NextHead>
